402f8d90f1b1f8ab1ec7e4baf6a78974820a0dc8
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-08-09 04:25:42 -0400

    changed colors and layout

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ use fltk::app;
 use fltk::frame::Frame;
 use fltk::enums::*;
 
-const APP_ID: &str = "org.physics_sim.rusty_pendulum";
+//const APP_ID: &str = "org.physics_sim.rusty_pendulum";
 
 fn main() -> Result<(), String> {
     // TODO: Use different window proportions
     let wnd_width: i32 = 1000;
     let wnd_height: i32 = 1000;
+    // Colors to use
+    let dark_brown: Color = Color::from_rgb(30, 24, 19);
+    let graph_paper: Color = Color::from_rgb(195, 190, 175);
     // Create application
-    //let app = app::App::default().with_scheme(app::Scheme::Plastic);
-    let app = app::App::default();
+    let app = app::App::default().with_scheme(app::Scheme::Oxy);
     // Create a window
     let mut wnd = window::Window::new(800, 200, wnd_width, wnd_height, "RustyPendulum");
     // TODO: Create a menubar
@@ -22,22 +24,23 @@ fn main() -> Result<(), String> {
     title.set_frame(FrameType::RoundedBox);
     title.set_label_font(Font::ScreenBold);
     title.set_label_size(30);
-    title.set_label_color(Color::from_rgb(55, 50, 42));
+    title.set_label_color(dark_brown);
     let label_dimensions: (i32, i32) = title.measure_label();
     title.set_size(label_dimensions.0 + 5, label_dimensions.1);
     //let title_w = title.clone().w();
     let title_h = title.clone().h();
-    title.set_color(Color::from_rgb(200, 200, 190));
+    title.set_color(graph_paper);
     title.center_x(&wnd);
     // Create a window to contain the simulation
-    let mut pendulum_wnd = window::Window::new(20, title_h + 4, 
-                    wnd_width - 50, wnd_height - title_h - 20, 
-                    None);
-    pendulum_wnd.set_color(Color::from_rgb(200, 200, 190));
+    let mut pendulum_wnd = window::Window::default()
+        .with_size(wnd_width - 50, wnd_height - title_h - 20)
+        .with_pos(0, title_h - 2);
+    pendulum_wnd.set_color(graph_paper);
     pendulum_wnd.end();
+    pendulum_wnd.center_x(&wnd);
 
     // Window settings details
-    wnd.set_color(Color::from_rgb(55, 50, 42));
+    wnd.set_color(dark_brown);
     wnd.make_resizable(false); // To prevent xmonad from tiling it
     wnd.end();
     wnd.show();
