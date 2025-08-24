424c863efcfb0b07d800ab65e94218f7183c4654
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-08-24 14:37:12 -0400

    added enum for approximation methods and fleshed out Pendulum struct

--- src/pendulum.rs
+++ src/pendulum.rs
@@ -1,22 +1,37 @@
 use fltk::prelude::*;
 use fltk::enums::Color;
 
-// TODO: add an enum for the type of approximation and add a member to 
-// pendulum struct 
+#[derive(PartialEq)]
+pub enum ApproximationMethods {
+    None,
+    Small_Angle,
+    Euler,
+    Euler_Cromer,
+    Runge_Kutta
+}
 
 pub struct Pendulum {
     theta: f32,
+    theta_dot: f32,
+    length: f32,
+    method: ApproximationMethods,
     color: Color
 }
 
 impl Pendulum {
-    pub fn new() -> Self {
+    pub fn new(length: f32) -> Self {
         Self {
             theta: 0f32, // Keep this
+            theta_dot: 0f32, // Keep this
+            length: length,
+            method: ApproximationMethods::None, // Probably keep this?
             color: Color::Red // temporary - replace this with command line argument
         }
     }
 
     // cartesian_coords function
-
+    pub fn cartesian_pos(&self) -> (i32, i32) {
+        ((self.length*self.theta.sin()).floor() as i32,
+        (self.length - self.length*self.theta.cos()).floor() as i32)
+    }
 }
